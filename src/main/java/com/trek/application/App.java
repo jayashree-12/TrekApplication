66cba7bfed1926f3fdd42352919d0f4e33896a56
venkatvp07 <venkat.prasath@zohocorp.com>
2025-04-18 05:29:14 +0530

    application ran

--- src/main/java/com/trek/application/App.java
+++ src/main/java/com/trek/application/App.java
@@ -1,19 +1,20 @@
 package com.trek.application;
 
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @ImportResource("spring.xml")
 public class App {
     public static void main(String[] args) {
     	String[] contextPaths = new String[] {"spring.xml"};
-//        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
-//        
-//        
-//        LearningFile learn = (LearningFile) factory.getBean("LearningFile");
-//        learn.hey = 10;
-//        learn.print();
-//        
-//        LearningFile learn1 = (LearningFile) factory.getBean("LearningFile");
-//        learn1.print();
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+
+        LearningFile learn = (LearningFile) factory.getBean("LearningFile");
+        learn.hey = 10;
+        learn.print();
+
+        LearningFile learn1 = (LearningFile) factory.getBean("LearningFile");
+        learn1.print();
     }
 }
