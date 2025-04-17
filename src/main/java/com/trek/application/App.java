f258fb373eb8524be558e2a2e55db00734f2d9ff
venkat.prasath <venkat.prasath@zohocorp.com>
2025-04-17 16:07:14 +0530

    jar changes

--- src/main/java/com/trek/application/App.java
+++ src/main/java/com/trek/application/App.java
@@ -1,10 +1,19 @@
 package com.trek.application;
 
-/**
- * Hello world!
- */
+import org.springframework.context.annotation.ImportResource;
+
+@ImportResource("spring.xml")
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+    	String[] contextPaths = new String[] {"spring.xml"};
+//        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+//        
+//        
+//        LearningFile learn = (LearningFile) factory.getBean("LearningFile");
+//        learn.hey = 10;
+//        learn.print();
+//        
+//        LearningFile learn1 = (LearningFile) factory.getBean("LearningFile");
+//        learn1.print();
     }
 }
